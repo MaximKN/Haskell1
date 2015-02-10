68a556cbf189c6c0abf2fe817288d2c95605ec57
Christopher O'Lenskie <colenskie@users.noreply.github.com>
2015-02-10 21:18:10 +0000

    Add missing files for file handling

---
+++ src/Helper.hs
@@ -0,0 +1,13 @@
+module Helper where
+
+-- Similar to function word, except it takes a predicate
+-- Src: http://stackoverflow.com/a/4981265/2849447
+wordsWhen     :: (Char -> Bool) -> String -> [String]
+wordsWhen p s =  case dropWhile p s of
+                      "" -> []
+                      s' -> w : wordsWhen p s''
+                            where (w, s'') = break p s'
+                            
+-- Read file from memory
+loadFile :: String -> IO String
+loadFile filename = readFile filename
