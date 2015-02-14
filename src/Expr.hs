0089d6ed6763c2d65a971adeb61771174fdba40b
Christopher O'Lenskie <colenskie@users.noreply.github.com>
2015-02-14 13:38:29 +0000

    Added mod, power, and abs)

--- src/Expr.hs
+++ src/Expr.hs
@@ -10,6 +10,9 @@ data Expr = Add Expr Expr
           | Val Float
           | Name Name
           | Err String
+		  | Abs Expr
+          | Mod Expr Expr
+          | Power Expr Expr
   deriving Show
 
 -- These are the REPL commands - set a variable name to a value, and evaluate
@@ -22,8 +25,7 @@ eval :: Tree (Name, Float) -> -- Variable name to value mapping
        Expr -> -- Expression to evaluate
         Either String Float -- Result (if no errors such as missing variables)
 
-eval _    (Val x)   | isInt x   = Right (toInt x)
-                    | otherwise = Right x
+eval _    (Val x)   = Right x
 eval vars (Add x y) = val (+)   vars x y
 eval vars (Sub x y) = val (-)   vars x y
 eval vars (Mul x y) = val (*)   vars x y
@@ -32,6 +34,15 @@ eval _    (Err msg) = Left msg
 eval vars (Name x)  = case getValueFromTree x vars of
                         Just a  -> Right a
                         Nothing -> Left "Use of undeclared variable"
+
+eval vars (Abs n) = Right (abs (fromRight (eval vars n)))
+
+eval vars (Mod x y) = case isInt (fromRight (eval vars x)) && isInt (fromRight (eval vars y)) of
+						True -> Right $ fromIntegral $ (toInteger $ round $ fromRight $ eval vars x) `mod` (toInteger $ round $ fromRight $ eval vars y)
+									--Right (((fromInteger (round (fromRight (eval vars x))))) `mod` ((fromInteger (round (fromRight (eval vars y))))))
+						False -> Left "Can't mod floats"
+						
+eval vars (Power x y) = Right ((fromRight (eval vars x)) ** (fromRight (eval vars y)))
 eval _ _ = Left "Couldn't evaluate expression" -- catch errors
 
 val :: (Float -> Float -> Float) -> Tree (Name, Float) -> Expr -> Expr -> Either String Float
@@ -60,13 +71,17 @@ pExpr = do t <- pTerm
 pFactor :: Parser Expr
 pFactor = do d <- float
              return (Val d)
-           ||| do v <- identifier
-                  return (Name v)
-                ||| do symbol "("
-                       e <- pExpr
-                       symbol ")"
-                       return e
-                     ||| return (Err ("Could not parse expression"))
+          ||| do v <- identifier
+                 return (Name v)
+              ||| do symbol "|"
+                     e <- pExpr
+                     symbol "|"
+                     return (Abs e)
+                  ||| do symbol "("
+                         e <- pExpr
+                         symbol ")"
+                         return e
+                      ||| return (Err ("Could not parse expression"))
 
 pTerm :: Parser Expr
 pTerm = do f <- pFactor
@@ -76,4 +91,10 @@ pTerm = do f <- pFactor
             ||| do symbol "/"
                    t <- pTerm
                    return (Div f t)
-                 ||| return f
+                 ||| do symbol "^"
+                        e <- pTerm
+                        return (Power f e)
+                    ||| do symbol "mod"
+                           e <- pTerm
+                           return (Mod f e) 
+                        ||| return f
