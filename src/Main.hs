853097d4df8615840d65adc9e9c6761c6ee48335
Christopher O'Lenskie <colenskie@users.noreply.github.com>
2015-02-15 18:22:51 +0000

    added haddock

--- src/Main.hs
+++ src/Main.hs
@@ -5,5 +5,5 @@ import REPL
 
 -- | Main loop
 main :: IO ()
-main = do hSetBuffering stdout NoBuffering -- ^ turn standard output buffering off so that the prompt flushes before getting input
+main = do hSetBuffering stdout NoBuffering -- turn standard output buffering off so that the prompt flushes before getting input
           repl initState
